347f89e46b9ac59efa6e41effddb76a36a210468
Rajiv <quadruplestak@gmail.com>
2021-06-10 08:47:13 -0400

    all problems added

---
+++ removeKthNodeFromEnd.py
@@ -0,0 +1,25 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+# O(n) time | O(1) space
+def removeKthNodeFromEnd(head, k):
+    counter = 1
+	first = head
+	second = head
+	while counter <= k:
+		second = second.next
+		counter += 1
+	if second is None:
+		head.value = head.next.value
+		head.next = head.next.next
+		return
+	while second.next is not None:
+		second = second.next
+		first = first.next
+	#first is pointing to the node right before the node we want to remove
+	#first.next = NODE_TO_REMOVE
+	#firs.next = NODE_TO_REMOVE.next
+	first.next = first.next.next
